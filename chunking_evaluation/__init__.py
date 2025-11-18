5010597caa8c9cf165bd54044f4d1c370cab24cd
Xiran Lu <xlu92427@gmail.com>
2025-11-17 21:34:59 -0800

    Remove unused folders and files

--- chunking_evaluation/__init__.py
+++ chunking_evaluation/__init__.py
@@ -1,10 +1,8 @@
-from .chunking.base_chunker import BaseChunker
-from .evaluation_framework.general_evaluation import GeneralEvaluation
+from .evaluation_framework.base_evaluation import BaseEvaluation
 from .evaluation_framework.synthetic_evaluation import SyntheticEvaluation
 from .utils import *
 
 __all__ = [
-    'BaseChunker',
-    'GeneralEvaluation',
+    'BaseEvaluation',
     'SyntheticEvaluation',
 ]
